775bcf288d2ed0966680ded0a3dc7b1b385b890f
Loic Rouaud <loic.rouaud@gmail.com>
2021-08-21 23:19:31 +0200

    Lesson-1 files modif

--- Lesson-1/Lesson-1_exercises.py
+++ Lesson-1/Lesson-1_exercises.py
@@ -1,1 +1,11 @@
 #Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+name = input("What is your name? ")
+print("Hello ", name)
+
+#Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+hours = int(input("Enter hours: "))
+rate = float(input("Enter rate: "))
+pay = hours * rate
+print("Your rate is $", pay)
+
+
