28bde605f1b047dad0db4b255730f482004f85a5
Loic Rouaud <loic.rouaud@gmail.com>
2021-08-21 23:48:50 +0200

    Lesson-2 files modif

---
+++ Lesson-2/Lesson-2_exercises.py
@@ -0,0 +1,23 @@
+#Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+name = input("What is your name? ")
+print("Hello ", name)
+
+#Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+hours = int(input("Enter hours: "))
+rate = float(input("Enter rate: "))
+pay = round((hours * rate), 2)
+print("Your rate is $", pay)
+
+#Exercise 4: Assume that we execute the following assignment statements:
+width =17
+height = 12.0
+print(type(width//2))
+print(type(width/2.0))
+print(type(height/3))
+print(type(1 + 2 * 5))
+
+#Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+temp_F = int(input("Enter a temperature in Farhenheit: "))
+temp_C = (temp_F - 32) * (5/9)
+round_temp_C = round(temp_C, 2)
+print(temp_F, "fahrenheit degrees is", round_temp_C, "in Celsius degrees")
