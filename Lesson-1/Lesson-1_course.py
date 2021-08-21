e85b67b774764c20cf7d8977ade35154de621030
Loic Rouaud <loic.rouaud@gmail.com>
2021-08-21 21:54:14 +0200

    Lesson-1 files modif

--- Lesson-1/Lesson-1_course.py
+++ Lesson-1/Lesson-1_course.py
@@ -1,3 +1,39 @@
 #Values and types
 print(4)
 
+#To have the type of a value
+type("Hello, World!")
+type(17)
+type(3.2)
+
+print(1,000,000)
+
+#Variables
+message = "And now for something completely different"
+n = 17
+pi = 3.14159
+
+print(n)
+print(pi)
+
+type(message)
+type(n)
+type(pi)
+
+#Variable names and keywords
+'''and       del       from      None      True
+as        elif      global    nonlocal  try
+assert    else      if        not       while
+break     except    import    or        with
+class     False     in        pass      yield
+continue  finally   is        raise     async
+def       for       lambda    return    await
+'''
+
+#Statements
+print(1)
+x = 2
+print(x)
+
+#Asking the user for input
+name = input('What is your name?\n')
---
+++ Lesson-1/Lesson-1_exercises.py
@@ -0,0 +1,1 @@
+#Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
