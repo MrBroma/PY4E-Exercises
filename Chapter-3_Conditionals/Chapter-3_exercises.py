6f1773e1899064ba3ba488b510ba80e6ba408ada
Loic Rouaud <loic.rouaud@gmail.com>
2021-09-12 00:54:47 +0200

    Chapter-3_Exercises

--- Chapter-3_Conditionals/Chapter-3_exercises.py
+++ Chapter-3_Conditionals/Chapter-3_exercises.py
@@ -12,16 +12,20 @@ print(pay)
 # Exercise 2: Rewrite your pay program using try and except so
 # that your program handles non-numeric input gracefully by printing
 # a message and exiting the program. The following shows two executions of the program:
-hr = int(input("Enter hours: "))
-rate = float(input("Enter the rate: "))
+hour2 = input("Enter hours: ")
+rate2 = input("Enter the rate: ")
 try:
-    if hr > 40:
-        pay = (40 * rate) + ((hr - 40) * (10.5 * 1.5))
-    else:
-        pay = hr * rate
-    print(pay)
+    hr = int(hour2)
+    r = float(rate2)
 except:
-    print("Enter a number")
+    print("Please enter a numeric number")
+
+if hr > 40:
+    pay = (40 * r) + ((hr - 40) * (10.5 * 1.5))
+else:
+    pay = hr * r
+
+print("Your pay is pay is ", pay)
 
 # Exercise 3: Write a program to prompt for a score between 0.0 and 1.0. If the score
 # is out of range, print an error message. If the score is between 0.0 and 1.0, print
