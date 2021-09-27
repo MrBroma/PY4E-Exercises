5baaebf50a24c1d8e4d2af9c6113c705d7be9d7a
Loic Rouaud <loic.rouaud@gmail.com>
2021-09-27 23:47:13 +0200

    Chapter-3_Exercises

--- Chapter-3_Conditionals/Chapter-3_exercises.py
+++ Chapter-3_Conditionals/Chapter-3_exercises.py
@@ -32,7 +32,7 @@ except:
 
 # Exercise 3: Write a program to prompt for a score between 0.0 and 1.0. If the score
 # is out of range, print an error message. If the score is between 0.0 and 1.0, print
-# a grade using the following table:
+# a grade using the following table
 
 score = float(input("Enter Score: "))
 if score >= 0.9:
