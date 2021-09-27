a98f861c62bff30f11b00005a73a7c203f2b0ef6
Loic Rouaud <loic.rouaud@gmail.com>
2021-09-27 23:44:04 +0200

    Chapter-4_Exercises

--- Chapter-4_Functions/Lesson-4_exercises.py
+++ Chapter-4_Functions/Lesson-4_exercises.py
@@ -48,4 +48,51 @@ def print_lyrics():
 
 repeat_lyrics()
 
-#
+# Exercise 4: What is the purpose of the “def” keyword in Python?
+
+# a) It is slang that means “the following code is really cool”
+# b It indicates the start of a function
+# c It indicates that the following indented section of code is to be stored for later
+# d b and c are both true
+# e) None of the above
+
+# Exercise 5: What will the following Python program print out?
+
+
+def fred():
+    print("Zap")
+
+
+def jane():
+    print("ABC")
+
+
+jane()
+fred()
+jane()
+
+# Exercise 6: Rewrite your pay computation with time-and-a-half for overtime and create a function called computepay
+# which takes two parameters (hours and rate).
+hr = int(input("Enter hours: "))
+rate = float(input("Enter the rate: "))
+if hr > 40:
+    pay = (40 * rate) + ((hr - 40) * (10.5 * 1.5))
+else:
+    pay = hr * rate
+
+print(pay)
+
+# Exercise 7: Rewrite the grade program from the previous chapter using a function called computegrade that takes
+# a score as its parameter and returns a grade as a string.
+
+score = float(input("Enter Score: "))
+if score >= 0.9:
+    print('Your score is A')
+elif score >= 0.8:
+    print('Your score is B')
+elif score >= 0.7:
+    print('Your score isC')
+elif score >= 0.6:
+    print('Your score is D')
+else:
+    print('Your score is F')
